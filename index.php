61505d240b0307351a74b73b2fb51786a7c55f5b
vercrue <e.vercruysse@gmail.com>
2012-08-15 18:01:01 +0200

    Renamed template.php to index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+	<title>Login &amp; Register</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/screen.css">
+</head>
+<body>
+	<header>
+		<h1 class="logo">Logo</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="downloads.php">Downloads</a></li>
+				<li><a href="forum.php">Forum</a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+			</ul>
+		</nav>
+		<div class="clear"></div>
+	</header>
+	<div id="container">
+		<aside>
+			<div class="widget">
+				<h2>Widget Header</h2>
+				<div class="inner">
+					Widget Contents
+				</div>
+			</div>
+		</aside>
+		<h1>Home</h1>
+		<p>Just a template.</p>
+	</div>
+	<footer>
+		&copy; ev-services.info 2012. All rights reserved.
+	</footer>
+</body>
+</html>
